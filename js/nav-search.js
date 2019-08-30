c44cb9468c23e52977296d4ce67a5b8d36170492
angel-barrera <angel.barrera@lakmisystems.com.mx>
2019-08-30 16:24:58 -0500

    sombra a search sin efecto hover

--- js/nav-search.js
+++ js/nav-search.js
@@ -19,6 +19,7 @@ if(barSearch && btnSearch){
          if(!evNav){
             barSearch.style.borderColor = 'var(--gray-light-bg)'
             barSearch.style.width = 'calc(98vw - 160px - 2*16px)'
+            barSearch.style.boxShadow = '0 0 .2rem .1rem rgba(255,255,255,0.25)'
             barSearch.firstElementChild.style.visibility = 'visible'
             barSearch.firstElementChild.style.width = '95%'
             evNav = true
@@ -37,5 +38,14 @@ if(barSearch && btnSearch){
          barSearch.firstElementChild.value = ''
          evNav = false
       }
+      if(innerWidth < 768){
+         if(e.target === barSearch.firstElementChild){
+            barSearch.style.boxShadow = '0 0 .2rem .1rem rgba(255,255,255,0.25)'
+         }
+         else{
+            barSearch.style.boxShadow = 'none'
+         }
+      }
+      
    })
 }
