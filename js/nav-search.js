a9fb5f3795e3c8dbbf7c557d86180d8a7b956277
angel-barrera <angel.barrera@lakmisystems.com.mx>
2019-09-03 15:06:02 -0500

    cerrar search presionando esc

--- js/nav-search.js
+++ js/nav-search.js
@@ -12,6 +12,10 @@ let btnSearch = document.getElementById('btn-search'),
 }
 */
 if(barSearch && btnSearch){
+   let closeSearch = () => {
+      barSearch.style = barSearch.firstElementChild.style = null
+      evNav = false
+   }
    /* Evento en boton de bar search */
    btnSearch.addEventListener('click', e => {
       e.preventDefault()
@@ -24,33 +28,37 @@ if(barSearch && btnSearch){
             barSearch.firstElementChild.style.visibility = 'visible'
             barSearch.firstElementChild.style.width = '95%'
             evNav = true
-         }else{
-            barSearch.style = barSearch.firstElementChild.style = null
-            evNav = false
-         }
+         }else closeSearch()
+         barSearch.firstElementChild.value = ''
+      }
+   })
+   /* Evento en bar search para cerrar barra sin hacer click en el input text */
+   barSearch.addEventListener('keyup', e => {
+      if(e.keyCode == 27){
+         closeSearch()
          barSearch.firstElementChild.value = ''
       }
    })
    /* Eventos en input text de bar search */
    barSearch.firstElementChild.addEventListener('click', e =>{
       e.stopPropagation()
-      if(innerWidth < 768){
+      if(innerWidth < 768)
          barSearch.style.boxShadow = '0 0 .2rem .1rem rgba(255,255,255,0.25)'
-      }
    })
    barSearch.firstElementChild.addEventListener('keyup', e =>{
-      console.log(`buscar resultados para: ${e.target.value}`)
-
+      e.stopPropagation()
+      console.log(`buscar resultados para ${e.keyCode}: ${e.target.value}`)
+      if(e.keyCode == 27){
+         closeSearch()
+         barSearch.firstElementChild.value = ''
+      }
    })
-   /* Evento en html para cerrar bar search */
-   document.addEventListener('click', e => {
-      if(innerWidth >= 768){
-         barSearch.style = barSearch.firstElementChild.style = null
-         evNav = false
-      }
-      else{
+   /* Evento en body para cerrar bar search */
+   document.documentElement.lastElementChild.addEventListener('click', e => {
+      if(innerWidth >= 768)
+         closeSearch()
+      else
          barSearch.style.boxShadow = 'none'
-      }
       barSearch.firstElementChild.value = ''
    })
 }
