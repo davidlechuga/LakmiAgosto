0b072374477e011bf0e4015e33d84cdc738b1598
angel-barrera <angel.barrera@lakmisystems.com.mx>
2019-09-13 17:41:18 -0500

    correccion condicional JS slider

--- js/slider.js
+++ js/slider.js
@@ -10,7 +10,7 @@ if(elemenTitle && itemsBG.length && itemsTitle.length && itemsArt.length && item
     n = itemsBG.length
 
     elemenTitle.addEventListener('transitionend',e => {
-        if((count == 0 || count == 4) && [... e.target.classList].indexOf('slide-prev'))
+        if((count == 0 || count == 4) && [... e.target.classList].indexOf('slide-prev')>=0)
             e.target.classList.remove('slide-prev')
     })
 
